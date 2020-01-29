beaf88c748eb4eaccc77e3fd2727b862b01677ff
Christopher Barnes <cbarnes7404@yahoo.com>
2020-01-29 14:09:38 -0800

    update templating (free to change)

--- Boroughs/urls.py
+++ Boroughs/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from Boroughs.views import index
+from Boroughs.views import Index, About
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', Index, name='index'),
+    path('About/', About, name='about'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Boroughs/views.py
+++ Boroughs/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import logout
 from django.urls import reverse_lazy
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
-def index(result):
-    return HttpResponse("Hello, world. You're at the Boroughs index.")
+def Index(request):
+    return render(request, 'index.html')
+
+def About(request):
+    return render(request, 'about.html')
