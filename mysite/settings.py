819bf0be15b15d2a9ff025b32177fbbdab412df2
Christopher Barnes <cbarnes7404@yahoo.com>
2020-02-10 14:07:35 -0800

    set collectstatic to debug 1

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'Boroughs',
-    'imagekit',
+    # 'imagekit',
 ]
 
 MIDDLEWARE = [
@@ -126,21 +126,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILE_DIRECTORY = 'static'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, STATICFILE_DIRECTORY)
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # BOROUGH app settings
 BOROUGH_TITLE_MAX_LENGTH = 200
