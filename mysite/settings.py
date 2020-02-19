7badf4b3c057aa57e12bd40cd92e87506e85faa4
Christopher Barnes <cbarnes7404@yahoo.com>
2020-02-18 22:44:42 -0800

    update style glitch from debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-USE_S3 = os.getenv('USE_S3', False) == True
+USE_S3 = os.getenv('USE_S3', False)
 
 print(USE_S3)
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -26,4 +26,8 @@ urlpatterns = [
     path('media/<str:path>', serve,
         {'document_root': settings.MEDIA_ROOT}),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
