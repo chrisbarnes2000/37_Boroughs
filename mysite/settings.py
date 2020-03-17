62c75d880d7211e9aa2be9b9a2d87df40b8df45d
Christopher Barnes <chris.barnes.2000@me.com>
2020-03-16 20:29:24 -0700

    [update] changing repos this is now old/outdated

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 # Required for Heroku
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'boroughs37.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'boroughs37-old.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
