bc21926c43175b56419d6b6acc91449707e177c1
Christopher Barnes <chris.barnes.2000@me.com>
2020-03-18 16:54:59 -0700

    exit debug maode update allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Required for Heroku
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'boroughs37-old.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'boroughs37.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
