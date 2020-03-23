bd5cbc7c64ef7536b587a1fcbd62d7565b6a3722
Christopher Barnes <chris.barnes.2000@me.com>
2020-03-23 14:47:24 -0700

    [add] account signup & login, [WIP] user accounts

--- users/views.py
+++ users/views.py
@@ -1,11 +1,18 @@
 # users/views.py
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
-
-from .forms import CustomUserCreationForm
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+# from .forms import CustomUserCreationForm
 
 
 class SignUp(CreateView):
-    form_class = CustomUserCreationForm
+    form_class = UserCreationForm
+    # form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
+
+
+class Profile(DetailView):
+    model = User
+    template_name = 'registration/profile.html'
